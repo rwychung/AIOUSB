ceddbb2c59babce852fe609d0d819cfeac9db429
Jimi Damon <jdamon@accesio.com>
2015-05-10 11:17:44 -0700

    REF: made aiousb_dio.h searchable for extern functions

--- AIOUSB/lib/AIOUSB_DIO.h
+++ AIOUSB/lib/AIOUSB_DIO.h
@@ -21,100 +21,26 @@ namespace AIOUSB
 #endif
 
 
-PUBLIC_EXTERN AIORESULT DIO_ConfigureWithDIOBuf(
-                                                unsigned long DeviceIndex,
-                                                unsigned char bTristate,
-                                                AIOChannelMask *mask,
-                                                DIOBuf *buf
-                                                );
+PUBLIC_EXTERN AIORESULT DIO_ConfigureWithDIOBuf( unsigned long DeviceIndex, unsigned char bTristate, AIOChannelMask *mask, DIOBuf *buf ); 
+PUBLIC_EXTERN unsigned long DIO_Configure( unsigned long DeviceIndex, unsigned char bTristate, void *pOutMask, void *pData ); 
+PUBLIC_EXTERN unsigned long DIO_ConfigureEx( unsigned long DeviceIndex, void *pOutMask, void *pData, void *pTristateMask ); 
+PUBLIC_EXTERN unsigned long DIO_ConfigurationQuery( unsigned long DeviceIndex, void *pOutMask, void *pTristateMask ); 
+PUBLIC_EXTERN unsigned long DIO_WriteAll( unsigned long DeviceIndex, void *pData ); 
+PUBLIC_EXTERN unsigned long DIO_Write8( unsigned long DeviceIndex, unsigned long ByteIndex, unsigned char Data ); 
 
-PUBLIC_EXTERN unsigned long DIO_Configure(
-                                          unsigned long DeviceIndex,
-                                          unsigned char bTristate,
-                                          void *pOutMask,
-                                          void *pData
-                                          );
+PUBLIC_EXTERN unsigned long DIO_Write1( unsigned long DeviceIndex, unsigned long BitIndex, unsigned char bData ); 
 
-PUBLIC_EXTERN unsigned long DIO_ConfigureEx( 
-                                            unsigned long DeviceIndex, 
-                                            void *pOutMask, 
-                                            void *pData, 
-                                            void *pTristateMask 
-                                             );
-
-PUBLIC_EXTERN unsigned long DIO_ConfigurationQuery(
-                                                   unsigned long DeviceIndex,
-                                                   void *pOutMask,
-                                                   void *pTristateMask
-                                                   );
-
-PUBLIC_EXTERN unsigned long DIO_WriteAll(
-                                         unsigned long DeviceIndex,
-                                         void *pData
-                                         );
-
-PUBLIC_EXTERN unsigned long DIO_Write8(
-                                       unsigned long DeviceIndex,
-                                       unsigned long ByteIndex,
-                                       unsigned char Data
-                                       );
-
-
-PUBLIC_EXTERN unsigned long DIO_Write1(
-                                       unsigned long DeviceIndex,
-                                       unsigned long BitIndex,
-                                       unsigned char bData
-                                       );
-
-
-PUBLIC_EXTERN AIORESULT DIO_ReadIntoDIOBuf(
-                                           unsigned long DeviceIndex,
-                                           DIOBuf *buf
-                                           );
-
+PUBLIC_EXTERN AIORESULT DIO_ReadIntoDIOBuf( unsigned long DeviceIndex, DIOBuf *buf ); 
 PUBLIC_EXTERN AIORESULT DIO_ReadAll( unsigned long DeviceIndex, void *buf );
 
 
-PUBLIC_EXTERN unsigned long DIO_ReadAllToCharStr(
-                                                 unsigned long DeviceIndex,
-                                                 char *buf,
-                                                 unsigned size
-                                                 );
-
-PUBLIC_EXTERN unsigned long DIO_Read8(
-                                      unsigned long DeviceIndex,
-                                      unsigned long ByteIndex,
-                                      int *pdat
-                                      );
-
-PUBLIC_EXTERN unsigned long DIO_Read1(
-                                      unsigned long DeviceIndex,
-                                      unsigned long BitIndex,
-                                      int *bit
-                                      );
-
-PUBLIC_EXTERN unsigned long DIO_StreamOpen(
-                                           unsigned long DeviceIndex,
-                                           unsigned long bIsRead
-                                           );
-
-PUBLIC_EXTERN unsigned long DIO_StreamClose(
-                                            unsigned long DeviceIndex
-                                            );
-
-PUBLIC_EXTERN unsigned long DIO_StreamSetClocks(
-                                                unsigned long DeviceIndex,
-                                                double *ReadClockHz,
-                                                double *WriteClockHz
-                                                );
-
-PUBLIC_EXTERN unsigned long DIO_StreamFrame(
-                                            unsigned long DeviceIndex,
-                                            unsigned long FramePoints,
-                                            unsigned short *pFrameData,
-                                            unsigned long *BytesTransferred
-                                            );
-
+PUBLIC_EXTERN unsigned long DIO_ReadAllToCharStr( unsigned long DeviceIndex, char *buf, unsigned size ); 
+PUBLIC_EXTERN unsigned long DIO_Read8( unsigned long DeviceIndex, unsigned long ByteIndex, int *pdat ); 
+PUBLIC_EXTERN unsigned long DIO_Read1( unsigned long DeviceIndex, unsigned long BitIndex, int *bit ); 
+PUBLIC_EXTERN unsigned long DIO_StreamOpen( unsigned long DeviceIndex, unsigned long bIsRead ); 
+PUBLIC_EXTERN unsigned long DIO_StreamClose( unsigned long DeviceIndex ); 
+PUBLIC_EXTERN unsigned long DIO_StreamSetClocks( unsigned long DeviceIndex, double *ReadClockHz, double *WriteClockHz ); 
+PUBLIC_EXTERN unsigned long DIO_StreamFrame( unsigned long DeviceIndex, unsigned long FramePoints, unsigned short *pFrameData, unsigned long *BytesTransferred );
 #ifdef __aiousb_cplusplus
 } // namespace
 #endif
