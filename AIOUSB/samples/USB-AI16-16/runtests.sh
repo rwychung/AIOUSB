3287f12cb0b34a937b0597f4329f08db6a2e2ec5
Jimi Damon <jdamon@accesio.com>
2015-06-18 13:59:34 -0700

    TST: added continuous_mode_callback to automated tests

--- AIOUSB/samples/USB-AI16-16/runtests.sh
+++ AIOUSB/samples/USB-AI16-16/runtests.sh
@@ -15,7 +15,8 @@ function run_cmd () {
 dynmake burst_test
 dynmake continuous_mode
 dynmake read_channels_test 
-dynmake bulk_acquire_sample                                                                                               
+dynmake bulk_acquire_sample
+dynmake continuous_mode_callback                                                                            
 
 
 run_cmd "./burst_test -V --num_scans 20000 --range 0-5=2 -c 20000 -D 31"
@@ -62,4 +63,16 @@ if [ $result == "not ok" ] ; then
     exit 1
 fi
 
+run_cmd "./continuous_mode_callback -V -N 16301 --range 0-3=2 -D 31 -S 200000"
+result=$(r -e 'tmp<-read.csv("output.txt",header=F); cat(if(max(tmp$V2) > 4.6) { "ok" } else { "not ok" } )')
+if [ $result == "not ok" ] ; then
+    echo "ERROR: max value not ok"
+    exit 1
+fi
+
+result=$(r -e 'tmp<-read.csv("output.txt",header=F); cat(if(min(tmp$V2) < 0.3) { "ok" } else { "not ok" } )')
+if [ $result == "not ok" ] ; then
+    echo "ERROR: min value not ok"
+    exit 1
+fi
 
