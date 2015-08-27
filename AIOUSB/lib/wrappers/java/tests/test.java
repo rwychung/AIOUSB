410d26e40af800a0797c74e27ac4ed00d1d5e272
Jimi Damon <jdamon@accesio.com>
2015-08-27 15:53:47 -0700

    TST: smarter test fix for java swig

--- AIOUSB/lib/wrappers/java/tests/test.java
+++ AIOUSB/lib/wrappers/java/tests/test.java
@@ -2,6 +2,7 @@
 
 import AIOUSB.*;
 
+
 public class test {
  static {
     System.loadLibrary("AIOUSB");
@@ -10,5 +11,10 @@ public class test {
   public static void main(String argv[]) {
     System.out.println(AIOUSB.AIOUSB_Init());
     AIOUSB.AIOUSB_ListDevices();
+    System.out.println("Result code is " + ResultCode.AIOUSB_ERROR_FILE_NOT_FOUND );
+
+    System.out.println( 4 == ResultCode.AIOUSB_ERROR_FILE_NOT_FOUND.swigValue() );
+    System.out.println( 0 == ResultCode.AIOUSB_SUCCESS.swigValue() );
+
   }
 }
