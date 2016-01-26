0d8c7a51f3015d4abdb908d54a93d1f1c5ea4046
Jimi Damon <jdamon@accesio.com>
2016-01-26 14:36:51 -0800

    TST: added sample for iiro boards

--- AIOUSB/samples/USB-IIRO-16_8/iiro_sample.c
+++ AIOUSB/samples/USB-IIRO-16_8/iiro_sample.c
@@ -1,29 +1,42 @@
-/** 
- *  @file iiro_sample.c
- *  @date $Format: %ad$$
- *  @author $Format: %an <%ae>$
- *  @version $Format: %h$
- *  @brief Sample program to demonstrate the behavior of the 
- *         USB-IIRO-8 or USB-IIRO-16 cards
+/**
+ * @file   iiro_sample.c
+ * @author $Format: %an <%ae>$
+ * @date   $Format: %ad$
+ * @version $Format: %h$
+ * 
+ * @page usb_iiro_sample iiro_sample.c
+ *
+ * @par iiro_sample
+ *
+ * iiro_sample.c is simple program that demonstrates using
+ * the IIRO relay based USB product line.
+ *
+ *
+ * @todo Complete this example
  */
 
+#ifndef IGNORE_DOXYGEN
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
 #include <time.h>
-
+#endif
 #include "aiousb.h"
 #include "aiocommon.h"
 
 #define RATE_LIMIT(product) \
     do {                                                                \
-        if( product >= USB_IIRO_16 && product <= USB_IDIO_16 )          \
+        if( product >= USB_IIRO_16 && product <= USB_IIRO_4 )           \
             sleep(1);                                                   \
     } while( 0 );                                                       \
 
-AIOUSB_BOOL fnd( AIOUSBDevice *dev ) { 
-    if ( dev->ProductID >=  USB_IIRO_16 && dev->ProductID <= USB_IDIO_16 ) { 
+AIOUSB_BOOL find_idio( AIOUSBDevice *dev ) { 
+    if( (dev->ProductID >= USB_IIRO_16 && dev->ProductID <= USB_IIRO_4 ) ||
+        (dev->ProductID >= USB_IDIO_16 && dev->ProductID <= USB_IDIO_4 )) {
+        /* found a USB-IIRO or USB-IDIO family device
+         * as this sample supports both
+         */
         return AIOUSB_TRUE;
     } else {
         return AIOUSB_FALSE;
@@ -32,50 +45,47 @@ AIOUSB_BOOL fnd( AIOUSBDevice *dev ) {
 
 int main(int argc, char *argv[] )
 {
+    AIODeviceQuery *devq;
+    int *indices, num_devices = 0, stopval;
+    int timeout = 1000;
+    int productId;
     struct opts options = AIO_OPTIONS;
-    unsigned long productID, nameSize, numDIOBytes, numCounters;
-    int stopval;
-    int *indices;
-    int num_devices;
     AIORET_TYPE retval;
-    int MAX_NAME_SIZE = 20;
-    char name[ MAX_NAME_SIZE + 2 ];
-    unsigned long deviceIndex = 0;
+    unsigned outData = 15;
 
-    nameSize = MAX_NAME_SIZE;
+    retval = AIOUSB_Init(); /**< Call AIOUSB_Init() first */
+    AIOUSB_ListDevices();   /**< Quickly list USB devices on the bus */
 
-
-    process_aio_cmd_line( &options, argc, argv );
-
-    AIOUSB_Init();
-    AIOUSB_ListDevices();
-    AIOUSB_FindDevices( &indices, &num_devices, fnd );
-
-    aio_list_devices( &options, indices, num_devices );
-
-    if( num_devices <= 0 ) {
-        fprintf(stderr,"No USB-IIRO cards were found\n" );
+    AIOUSB_FindDevices( &indices, &num_devices, find_idio );
+    if ( num_devices <= 0 ) {
+        fprintf(stderr,"Unable to find a USB-IDIO device\n");
         exit(1);
     }
-    
-    retval = QueryDeviceInfo( options.index, &productID, &nameSize, name, &numDIOBytes, &numCounters );
 
+    if ( ( retval = aio_supply_default_command_line_settings(&options)) != AIOUSB_SUCCESS )
+        exit(retval);
 
-    if ( productID  == 0x8018 || productID == USB_IIRO_16  ) { 
+    devq = NewAIODeviceQuery( options.index );
+    if (!devq ) {
+        fprintf(stderr,"Unable to query device at index %d \n", options.index );
+        exit(1);
+    }
+
+    if ( AIODeviceQueryGetProductID( devq ) == USB_IDIO_16 || AIODeviceQueryGetProductID(devq) == USB_IIRO_16  ) { 
         stopval = 16;
     } else { 
         stopval = 8;
     }
 
-    int timeout = 1000;
-    AIOUSB_Reset( deviceIndex );
-    AIOUSB_SetCommTimeout( deviceIndex, timeout );
-    unsigned outData = 15;
+    productId = AIODeviceQueryGetProductID( devq );
 
-    retval = DIO_WriteAll( deviceIndex, &outData );
+    AIOUSB_Reset( options.index );
+    AIOUSB_SetCommTimeout( options.index, timeout );
+
+    DIO_WriteAll( options.index, &outData );
 
 
-    if( (productID >= USB_IIRO_16 && productID <= USB_IIRO_4 )) {
+    if( ( productId >= USB_IIRO_16 && productId <= USB_IIRO_4 )) {
         goto walkingbit_test;
     }
     /* Speed test for IDIOs not for mechanical switching !! */
@@ -86,15 +96,15 @@ int main(int argc, char *argv[] )
         gettimeofday( &start, NULL ) ;
         if( 1 ) { 
             for ( outData = 0; outData < stopval ; outData ++  ) {
-                DIO_WriteAll( deviceIndex, &outData );
-                RATE_LIMIT( productID );
+                DIO_WriteAll( options.index, &outData );
+                RATE_LIMIT( productId );
                 tot ++;
             }
   
             for ( outData = 0; outData < stopval; outData ++ , tot ++ ) {
                 unsigned output = 1 << outData;
-                DIO_WriteAll( deviceIndex, &output );
-                RATE_LIMIT( productID );
+                DIO_WriteAll( options.index, &output );
+                RATE_LIMIT( productId );
                 tot++;
             }
             gettimeofday( &now, NULL ) ;
@@ -104,36 +114,51 @@ int main(int argc, char *argv[] )
  
  walkingbit_test:
    outData = 0x0; 
-   retval = DIO_WriteAll(deviceIndex, &outData );
+   DIO_WriteAll(options.index, &outData );
    unsigned output = 0;
    for ( outData = 0; outData < stopval; outData ++ ) {
        output |= (unsigned)(1 << outData);
-       retval = DIO_WriteAll( deviceIndex, &output );
-       RATE_LIMIT( productID );
+       DIO_WriteAll( options.index, &output );
+       RATE_LIMIT( productId );
    }
 
    DIOBuf *buf= NewDIOBuf(0);
    unsigned char cdat;
-   retval = DIO_ReadIntoDIOBuf( deviceIndex, buf );
+
+   DIO_Read8( options.index, 0, &cdat  );
+   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
+   DIO_Read8( options.index, 1, &cdat  );
+   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
+   DIO_Read8( options.index, 2, &cdat  );
+   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
+   DIO_Read8( options.index, 3, &cdat   );
+   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
+
+   outData = 43690;
+   DIO_WriteAll(options.index, &outData );
+
+   DIO_Read8( options.index, 0, &cdat  );
+   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
+   DIO_Read8( options.index, 1, &cdat  );
+   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
+   DIO_Read8( options.index, 2, &cdat  );
+   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
+   DIO_Read8( options.index, 3, &cdat   );
+   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
+
+   DIO_ReadIntoDIOBuf( options.index, buf );
    printf("Binary was: %s\n", DIOBufToString( buf ) );
    printf("Hex was: %s\n", DIOBufToHex( buf ) );
-   DIO_Read8( deviceIndex, 0, &cdat  );
-   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
-   DIO_Read8( deviceIndex, 1, &cdat  );
-   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
-   DIO_Read8( deviceIndex, 2, &cdat  );
-   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
-   DIO_Read8( deviceIndex, 3, &cdat   );
-   printf("Single data was : hex:%x, int:%d\n", (int)cdat, (int)cdat );
+
 
    unsigned char val=0;
    for ( int i = 7 ; i >= 0 ; i-- ) {
-       DIO_Read1(deviceIndex,i, &val);
+       DIO_Read1(options.index,i, &val);
        printf("%d", val );
    }
    printf("\n-----\n");
    for ( int i = 15 ; i >= 8 ; i -- ) {
-       DIO_Read1(deviceIndex,i, &val);
+       DIO_Read1(options.index,i, &val);
        printf("%d", val );
    }
    printf("\n");
