9680a78a7efbe8220aca3a35b08ff20378b59fb1
Jimi Damon <jdamon@accesio.com>
2016-03-23 12:45:01 -0700

    DOC: Documentation for AIOFifo

--- AIOUSB/lib/AIOFifo.h
+++ AIOUSB/lib/AIOFifo.h
@@ -40,19 +40,40 @@ AIORET_TYPE AIOFifoWrite( AIOFifo *fifo, void *frombuf , unsigned maxsize );
     volatile unsigned int read_pos;                                                  \
     volatile unsigned int write_pos;                                                 \
     AIO_EITHER_TYPE kind;                                                            \
-    AIORET_TYPE (*Read)( struct AIOFifo *fifo, void *tobuf, unsigned maxsize );     \
-    AIORET_TYPE (*Write)( struct AIOFifo *fifo, void *tobuf, unsigned maxsize );    \
+    AIORET_TYPE (*Read)( struct AIOFifo *fifo, void *tobuf, unsigned maxsize );      \
+    AIORET_TYPE (*Write)( struct AIOFifo *fifo, void *tobuf, unsigned maxsize );     \
     void (*Reset)( void *fifo );                                                     \
-    size_t (*delta)( struct AIOFifo *fifo  );                                       \
-    size_t (*rdelta)( struct AIOFifo *fifo  );                                      \
-    size_t (*_calculate_size_write)( struct AIOFifo *fifo, unsigned maxsize );      \
+    size_t (*delta)( struct AIOFifo *fifo  );                                        \
+    size_t (*rdelta)( struct AIOFifo *fifo  );                                       \
+    size_t (*_calculate_size_write)( struct AIOFifo *fifo, unsigned maxsize );       \
     size_t (*_calculate_size_read)( struct AIOFifo *fifo, unsigned maxsize );
 
     /* void (*Reset)( struct aio_fifo *fifo );                                          \ */
 
+/**
+ * @brief AIOFifo is a base class that is also instantiable for
+ * creating simple fifos for performing fast data acquisition.  The
+ * definition of the structure is comprised of the base interface (
+ * created wtih a \#define ) in AIO_FIFO_INTERFACE which handles the
+ * basic read and writing to the fifo. In addition , it also includes
+ * the Interface called LOCKING_MECHANISM, that makes sure that a
+ * write access to the FIFO is atomic. 
+ * 
+ * 
+ */
 typedef struct AIOFifo { 
-    AIO_FIFO_INTERFACE;         /**< The Main FIFO part    */
-    LOCKING_MECHANISM;          /**< The locking mechanism */
+    /**
+     * @brief The Interface for the FIFO that describes 
+     * the read / write and size functions that AIOFifo should provide
+     */
+    AIO_FIFO_INTERFACE;
+    
+    /**
+     * @brief The Interface for the Locking mechanism that defines
+     * GRAB_RESOURCE and RELEASE_RESOURCE
+     */
+
+    LOCKING_MECHANISM;
 } AIOFifo;
 
 
@@ -165,9 +186,15 @@ AIORET_TYPE AIOFifo##NAME ##Resize( AIOFifo##NAME *nfifo , size_t size )        
 }
 
 
-/* Counts Fifo definition */
+/**
+ * @brief Counts Fifo definition that is a Fifo of 2 byte count values
+ */
 TEMPLATE_AIOFIFO_INTERFACE(Counts,uint16_t);
-/* Volts Fifo definition */
+
+/**
+ * @brief Volts Fifo definition that is a Fifo of 8 byte double values
+ * that will be analog voltage readings
+ */
 TEMPLATE_AIOFIFO_INTERFACE(Volts,double);
 
 
