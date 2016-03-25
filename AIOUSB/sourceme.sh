4ef33dcb21391741c5b715f542101d21260a6abd
Jimi Damon <jdamon@accesio.com>
2016-03-25 10:28:30 -0700

    DEP: deprecated classlib and original java
    - now in the deprecated folder

--- AIOUSB/sourceme.sh
+++ AIOUSB/sourceme.sh
@@ -3,5 +3,5 @@
 
 export AIOUSB_ROOT="$(pwd)"
 export AIO_LIB_DIR="${AIOUSB_ROOT}/lib"
-export AIO_CLASSLIB_DIR="$AIOUSB_ROOT/classlib"
+export AIO_CLASSLIB_DIR="$AIOUSB_ROOT/deprecated/classlib"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${AIOUSB_ROOT}/lib:${AIOUSB_ROOT}/classlib"
