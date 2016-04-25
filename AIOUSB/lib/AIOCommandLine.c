17bb1c32db16f05e731c31963495246c42a8cfdd
Jimi Damon <jdamon@accesio.com>
2016-04-25 16:48:17 -0700

    ENH: fixed mem leak with arg parsing

--- AIOUSB/lib/AIOCommandLine.c
+++ AIOUSB/lib/AIOCommandLine.c
@@ -335,6 +335,7 @@ AIORET_TYPE AIOProcessCommandLine( AIOCommandLineOptions *options, int *argc, ch
         }
         *argc = keepcount;
         optind = keepcount + 1;
+        free(keepindices);
     }
 
     return AIOUSB_SUCCESS;
