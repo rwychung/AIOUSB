fa72cead7cea19b6e8f04e52555816bc4fa6c38c
Jimi Damon <jdamon@accesio.com>
2016-08-13 14:24:02 -0700

    TST: test java file that can load shared library

--- AIOUSB/lib/wrappers/java/tests/test.java
+++ AIOUSB/lib/wrappers/java/tests/test.java
@@ -1,20 +1,23 @@
 // runme.java
 
 import AIOUSB.*;
+import com.jdotsoft.jarloader.JarClassLoader;
 
 
 public class test {
- static {
-    System.loadLibrary("AIOUSB");
-  }
 
-  public static void main(String argv[]) {
-    System.out.println(AIOUSB.AIOUSB_Init());
-    AIOUSB.AIOUSB_ListDevices();
-    System.out.println("Result code is " + ResultCode.AIOUSB_ERROR_FILE_NOT_FOUND );
+    public static void main(String argv[]) {
+        System.out.println("Loading AIOUSB64");
+        System.loadLibrary("aiousbdbg");
+        System.loadLibrary("AIOUSB64");
 
-    System.out.println( 4 == ResultCode.AIOUSB_ERROR_FILE_NOT_FOUND.swigValue() );
-    System.out.println( 0 == ResultCode.AIOUSB_SUCCESS.swigValue() );
+        System.out.println( AIOUSB.AIOUSB_Init() );
 
-  }
+        AIOUSB.AIOUSB_ListDevices();
+        System.out.println("Result code is " + ResultCode.AIOUSB_ERROR_FILE_NOT_FOUND );
+        
+        System.out.println( 4 == ResultCode.AIOUSB_ERROR_FILE_NOT_FOUND.swigValue() );
+        System.out.println( 0 == ResultCode.AIOUSB_SUCCESS.swigValue() );
+        
+    }
 }
