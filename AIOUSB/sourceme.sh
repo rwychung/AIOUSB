bfdcf88eedd4b75744db19f2e216755e9149cf2a
Jimi Damon <jdamon@accesio.com>
2016-11-17 09:51:39 -0800

    ENH: fix for LD_LIBRARY_PATH to include deprecated

--- AIOUSB/sourceme.sh
+++ AIOUSB/sourceme.sh
@@ -4,6 +4,6 @@
 export AIOUSB_ROOT="$(pwd)"
 export AIO_LIB_DIR="${AIOUSB_ROOT}/lib"
 export AIO_CLASSLIB_DIR="$AIOUSB_ROOT/deprecated/classlib"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${AIOUSB_ROOT}/lib:${AIOUSB_ROOT}/classlib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${AIOUSB_ROOT}/lib:${AIOUSB_ROOT}/deprecated/classlib"
 export TEXINPUTS=$TEXINPUTS:$PWD/doc://;
 export PATH_TO_LIBUSB_SRC=$AIO_LIB_DIR/ndkbuild/libusb
